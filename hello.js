c20eebc7e13145135c15c4af85dfb3202a5c5dac
zaczacharydavis <zaczacharydavis@gmail.com>
2023-09-17 17:18:49 -0500

    hello world works

--- hello.js
+++ hello.js
@@ -11,7 +11,8 @@ Next, PLAY!
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
+   2. Figure out what the 
+   parentheses do. Will the code work without them?
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
    4. Remove the semi-colon, ;.
    5. Print a number. (Bonus: Print two numbers added together).
